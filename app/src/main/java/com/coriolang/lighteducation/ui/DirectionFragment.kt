2585d79258814a62a73024d39edf3173a7dbaadd
coriolang <yan.radi@gmail.com>
2023-01-13 22:15:59 +0300

    Create topic dialog

--- app/src/main/java/com/coriolang/lighteducation/ui/DirectionFragment.kt
+++ app/src/main/java/com/coriolang/lighteducation/ui/DirectionFragment.kt
@@ -2,11 +2,14 @@ package com.coriolang.lighteducation.ui
 
 import android.os.Bundle
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
 import com.coriolang.lighteducation.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DirectionFragment : Fragment() {
 
@@ -37,7 +40,25 @@ class DirectionFragment : Fragment() {
             }
         }, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_direction, container, false)
+        val view = inflater.inflate(R.layout.fragment_direction, container, false)
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab_create_topic)
+
+        fab.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setIcon(ContextCompat.getDrawable(
+                    requireContext(), R.drawable.ic_round_chat
+                ))
+                .setTitle("New topic")
+                .setView(R.layout.dialog_topic)
+                .setNegativeButton("Cancel") { dialog, which ->
+                    dialog.cancel()
+                }
+                .setPositiveButton("Create") { dialog, which ->
+                    // Respond to positive button press
+                }
+                .show()
+        }
+
+        return view
     }
 }
